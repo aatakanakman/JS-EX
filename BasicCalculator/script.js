c4a0d954bcd1327d04e144e4c6c75f7baa182a95
aatakanakman <48566734+aatakanakman@users.noreply.github.com>
2021-04-05 16:39:35 +0300

    BasicCalculator

---
+++ BasicCalculator/script.js
@@ -0,0 +1,99 @@
+const display = document.querySelector('.calculator-input');
+const keys = document.querySelector('.calculator-keys')
+
+let displayValue = '0';
+let firstValue = null;
+let operator = null;
+let waitingForSecondValue = false;
+
+updateDisplay();
+
+function updateDisplay(){
+    display.value= displayValue;
+}
+
+keys.addEventListener('click',function(e){
+
+    const element = e.target;
+    const value = element.value;
+    
+    if(!element.matches('button')) return; //tıklanan element button değil ise işlemleri durdur.
+
+    switch (value) {
+        case '+':
+        case '-':
+        case '*':
+        case '/':
+        case '=':
+            handleOperator(value)
+            break;
+        case '.':
+            inputDecimal();
+            break;
+        case 'clear':
+            clear();
+            break;
+        default:
+            inputNumber(value);
+    }
+    updateDisplay();
+})
+
+function inputNumber(num){
+    if(waitingForSecondValue == true){
+        displayValue = num;
+        waitingForSecondValue = false;
+    }else{
+        displayValue = displayValue === '0'? num : displayValue + num;
+    }
+    console.log(displayValue,firstValue,operator,waitingForSecondValue);
+}
+
+function inputDecimal(){
+    if(!displayValue.includes(".")){
+        displayValue += '.';
+    }
+}
+
+function clear(){
+    displayValue = "0";
+}
+
+function handleOperator(nextOperator){
+    
+    const value = parseFloat(displayValue);
+
+    if(operator && waitingForSecondValue){
+        operator = nextOperator;
+        return;
+    }
+
+    if(firstValue == null){
+        firstValue = value;
+    }else if(operator){
+        const result = calculate(firstValue,value,operator);
+
+        displayValue = `${parseFloat(result.toFixed(7))}`;
+        firstValue = result;
+    }
+
+    waitingForSecondValue = true
+    operator = nextOperator;
+
+    console.log(displayValue,firstValue,operator,waitingForSecondValue);
+}
+
+function calculate(first,second,opr){
+    
+    if(operator === '+'){
+        return first + second;
+    }else if(operator === '-'){
+        return first - second
+    }else if(operator === '*'){
+        return first * second;
+    }else if(operator === '/'){
+        return first / second;
+    }
+
+    return second;
+}
